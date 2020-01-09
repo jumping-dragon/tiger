3ada3eccf3e9a53303ad319f13660fdb428683d1
Alvin Januar <alvinjanuar234@ymail.com>
2020-01-10 01:29:02 +0800

    Update

--- init.sql
+++ init.sql
@@ -27,3 +27,17 @@ INSERT INTO `restaurant-accounts` (`id`, `name`, `email`,`password`) VALUES (1, 
 
 ALTER TABLE `restaurant-accounts` ADD PRIMARY KEY (`id`);
 ALTER TABLE `restaurant-accounts` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+
+CREATE TABLE IF NOT EXISTS `restaurant-menus` (
+  `menu_id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `price` int(50) NOT NULL,
+  `password` varchar(255) NOT NULL
+)ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `user-orders` (
+  `order_id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `restaurant_id` int(50) NOT NULL,
+  `price` varchar(255) NOT NULL,
+)ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
